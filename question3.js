482f47f1a269b9e0ba08dfb6291ba27d90d599f9
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-03-31 21:49:17 +0300

    3

--- question3.js
+++ question3.js
@@ -3,7 +3,7 @@ let array = [];
 //we have introduced tow variables, num (which is undefined) and array which is an array
 function primeno(num){
  if(num<=1){
-    return false
+    return false;
 }
 
 
